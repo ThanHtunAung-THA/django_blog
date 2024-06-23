05973aa20866345694537511e77a3d4d21c9cb67
BuggyCody <31753974+ThanHtunAung-THA@users.noreply.github.com>
2024-06-23 15:42:45 +0630

    post linst, create

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-  
+      path('', views.post_list, name='post_list'),
+      path('create/', views.post_create, name='post_create'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from blog.models import PostModel
 
 # Create your views here.
+# view
+def post_list(request):
+  # posts = PostModel.objects.all().order_by('-created_at')
+  posts = PostModel.objects.all().order_by('-created_at')
+  return render(request, 'postList.html', {"posts" : posts})
+
+# create
+def post_create(request):
+  if request.method == "GET":
+    return render(request, 'postCreate.html')
+  if request.method == "POST":
+    posts = PostModel.objects.create(
+    title = request.POST.get('title'),
+    body = request.POST.get('body'),
+    created_at = request.POST.get('created')
+    )
+    posts.save()
+    return redirect('/blog/')
