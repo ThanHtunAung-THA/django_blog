4a4de5d072af941ce21f9f2e9987ad4a4b181b83
BuggyCody <31753974+ThanHtunAung-THA@users.noreply.github.com>
2024-06-30 15:06:55 +0630

    detail , update, messages box

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,9 @@ from . import views
 urlpatterns = [
       path('', views.post_list, name='post_list'),
       path('create/', views.post_create, name='post_create'),
+      path('detail/<int:post_id>/', views.post_detail, name='post_detail'),
+      path('update/<int:post_id>/', views.post_update, name='post_update'),
+      path('delete/<int:post_id>/', views.post_delete, name='post_delete'),
+
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from blog.models import PostModel
+from django.contrib import messages
+import datetime
 
 # Create your views here.
 # view
@@ -19,4 +21,31 @@ def post_create(request):
     created_at = request.POST.get('created')
     )
     posts.save()
+    return redirect('/blog/')
+
+#post Detail
+def post_detail(request, post_id):
+    posts = PostModel.objects.get(id=post_id)
+    return render(request, 'postDetail.html', {"posts":posts})
+
+#post Update
+def post_update(request, post_id):
+    post = get_object_or_404(PostModel, id=post_id)
+    if request.method == "GET":
+        post.created_at = post.created_at.strftime('%Y-%m-%dT%H:%M')
+        return render(request, 'postUpdate.html', {"post": post})
+    elif request.method == "POST":
+        post.title = request.POST.get('title')
+        post.body = request.POST.get('body')
+        created_at_str = request.POST.get('created')
+        if created_at_str:
+            post.created_at = datetime.datetime.strptime(created_at_str, '%Y-%m-%dT%H:%M')
+        post.save()
+        return redirect('/blog/')
+
+#post delete
+def post_delete(request, post_id):
+    posts = PostModel.objects.filter(id = post_id)
+    posts.delete()
+    messages.success(request, "The post has been deleted successfully.")
     return redirect('/blog/')
