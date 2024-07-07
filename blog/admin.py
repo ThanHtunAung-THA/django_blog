9735ddbb8cd42060a49ac74c5a8ec786051a013a
BuggyCody <31753974+ThanHtunAung-THA@users.noreply.github.com>
2024-07-07 15:22:54 +0630

    added category model and refix db

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from blog.models import CategoryModel
 from blog.models import PostModel
 # Register your models here.
+admin.site.register(CategoryModel)
 admin.site.register(PostModel)
--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,25 @@
 from django.db import models
 from datetime import datetime
+from django.contrib.auth.models import User
 
 # Create your models here.
 
+class CategoryModel(models.Model):
+  id  = models.AutoField(primary_key=True)
+  name = models.CharField(max_length=100)
+  created_at = models.DateTimeField(default=datetime.now)
+
+  def __str__(self):
+    return self.name
+
 class PostModel(models.Model):
   id = models.AutoField(primary_key=True)
+  category = models.ForeignKey(CategoryModel, on_delete=models.CASCADE, default=None)
+  author = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+  image = models.ImageField(default=None, blank=True)
   title = models.CharField(max_length=100)
   body = models.TextField()
   created_at = models.DateTimeField(default=datetime.now)
+
+  def __str__(self):
+    return self.title
